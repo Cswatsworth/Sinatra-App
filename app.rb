f41bad30ae0907cf263c6659e83aaeee064a266c
Chad Swatsworth <Chad Swatsworth>
2017-02-20 11:18:14 -0500

    Working!!!!!!

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require_relative 'addition.rb'
+# require_relative 'addition.rb'
 
 get '/' do  
 
@@ -21,15 +21,29 @@ end
 
 post '/favorite' do
 	user_name = params[:user_name]
-	user_age = params[:user_age]
+	user_age = params[:user_age].to_i
 	user_num1 = params[:user_num1].to_i
 	user_num2 = params[:user_num2].to_i
 	user_num3 = params[:user_num3].to_i
+	sum = (user_num1 + user_num2 + user_num3)
 
-	favorite_total = sum(user_num1, user_num2, user_num3) 
-	results = difference(user_age, favorite_total)
+	results = 'less than'
 
-erb :results, :locals => {:user_name => user_name, :user_age => user_age, :user_num1 => user_num1, :user_num2 => user_num2, :user_num3 => user_num}
+		if (sum > user_age)
+				results = 'greater than'
+		elsif (sum == user_age)
+				results = 'equal to'
+		else
+				results
+		end
+
+	#numbers = params[:favorite].split(",")map { |s| s.to_i}.inject(0 :+)
+
+
+	# favorite_total = sum(user_num1, user_num2, user_num3) 
+	# results = difference(user_age, favorite_total)
+
+erb :results, :locals => {:user_name => user_name, :user_age => user_age, :user_num1 => user_num1, :user_num2 => user_num2, :user_num3 => user_num3, :sum => sum, :results => results}
 end
 # Favorite Number Kata:
 # Create a app using Sinatra which will accept/pass variables using locals. Create 4 views
